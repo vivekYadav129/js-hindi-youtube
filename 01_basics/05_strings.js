c0ec2721d87afb0732a75fb9bd5ebf97cfab2c39
vivekYadav129 <V8004282@gmail.com>
2025-09-07 07:13:30 +0000

    Added string example in JS

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,41 @@
+const name = "hitesh"
+const repoCount = 50
+
+// console.log(name + repoCount + "Value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount} `);
+
+
+// const gameName = new String('hiteshhhx')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName);
+// console.log(gameName.toLocaleUpperCase);
+console.log(gameName.charAt('t'));
+
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+
+const anotherString = gameName.slice(-8,4)
+console.log(anotherString);
+
+
+
+const newStringOne = "     hitesh    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+
+
+
+
+
+
+
+
+
+
