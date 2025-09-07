68943bcf43a8806c57ad93db1ab9dea3e00771cd
vivekYadav129 <V8004282@gmail.com>
2025-09-07 10:43:44 +0000

    array

---
+++ 02_basics/02_array.js
@@ -0,0 +1,35 @@
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+
+// const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros);
+
+const all_new_heros = [...marvel_heros, ...dc_heros]
+console.log(all_new_heros);
+
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array);
+
+
+
+console.log(Array.isArray("Hitesh"));
+console.log(Array.from("Hitesh"));
+console.log(Array.from({name: "hitesh"}))
+
+
+let score1 = 100
+let score2 = 200
+
+console.log(Array.of(score1, score2));
+
+
+
+
+
+
